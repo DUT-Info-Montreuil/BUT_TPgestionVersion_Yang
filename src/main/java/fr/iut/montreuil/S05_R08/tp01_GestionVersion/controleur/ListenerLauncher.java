4c3a0b459f49629f679423bace6d7f43acb466c3
G8LD <steven.yang.ys@gmail.com>
2023-09-27 10:14:43 +0200

    peut passer au niveau suivant en regardant une pub mais pas les pas, ne peut pas charger la map

--- src/main/java/fr/iut/montreuil/S05_R08/tp01_GestionVersion/controleur/ListenerLauncher.java
+++ src/main/java/fr/iut/montreuil/S05_R08/tp01_GestionVersion/controleur/ListenerLauncher.java
@@ -50,8 +50,8 @@ public class ListenerLauncher {
                 switch (joueur.getNumeroZone()){
                     case "0":
                         if(joueur.isCollinding(9*16, 16) && !joueur.getListeQuetes().getQueteActuelle().getTitre().startsWith("Chapitre 0"))  {
-                        	joueur.getConsole().afficherJeuHacked();
-                        	//terrainVue.loadMap("1", 36*16, 3*16);
+                        	//joueur.getConsole().afficherJeuHacked();
+                        	terrainVue.loadMap("1", 36*16, 3*16);
                         }
                         break;
                     case "1":
